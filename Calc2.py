c2457934e3b5091294fc42912e198de48ec35054
Mopuri Bhavik Sai <bhavik.mopuri@gmail.com>
2026-09-15 12:36:30 +0530

    Update Calc2.py

--- Calc2.py
+++ Calc2.py
@@ -1,10 +1,8 @@
-# This function adds two numbers
 import Addition
 import Subtraction
 import Multiplication
 import Division
 
-# This is to present a menu to the user
 print("Select operation.")
 print("1.Addition")
 print("2.Subtract")
@@ -12,10 +10,7 @@ print("3.Multiply")
 print("4.Divide")
 
 while True:
-    # take input from the user
     choice = input("Enter choice(1/2/3/4): ")
-
-    # check if choice is one of the four options
     if choice in ('1', '2', '3', '4'):
         try:
             num1 = float(input("Enter first number: "))
